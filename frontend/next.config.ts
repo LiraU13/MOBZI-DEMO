90afd905303db203b25d0ef03be49fe064256296
LiraU13 <moredialiraulises13@gmail.com>
2025-12-10 18:32:48 -0600

    Redireccionamiento en next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,15 @@ import path from 'path';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
